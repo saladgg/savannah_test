0d79351a7df7c109cb2c603154e8d1440e917364
salad <saladguyo60@gmail.com>
2021-03-03 14:18:54 +0300

    decouple

--- savannah_test/settings.py
+++ savannah_test/settings.py
@@ -1,11 +1,12 @@
 import os
 from datetime import timedelta
+from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = "b95t^2q+f5&vx8oga&v_+@(b$p5kh62-7dsrqhliuevuo3c688"
+# SECRET_KEY = "b95t^2q+f5&vx8oga&v_+@(b$p5kh62-7dsrqhliuevuo3c688"
 DEBUG = False
 
-# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 # DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = [
